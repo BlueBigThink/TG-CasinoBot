4c04f84c11099713d9568a67b754170a2fe47ed7
bluebigthink <bluebigthink@gmail.com>
2024-07-12 00:50:47 -0400

    generate hash

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from flask_mysqldb import MySQL
 from flask_cors import CORS
 import hashlib
 import random
+import jwt
+import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -12,12 +14,28 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'DB_AleekkCasino'
+app.config['SECRET_KEY'] = 'bluebigthinksecretkey' #BOT_TOKEN
 
 mysql = MySQL(app)
 
-secret_seed = str(random.randint(1, 1000000))
-nonce = str(random.randint(1, 1000000))
-hash_value = hashlib.sha256((secret_seed + nonce).encode()).hexdigest()
+def generateToken(userId):
+    return jwt.encode({
+        'user_id': userId,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+    }, app.config['SECRET_KEY'], algorithm="HS256")
+
+def verifyExpiredToken(token):
+    try:
+        data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+    except jwt.ExpiredSignatureError:
+        return False
+    return True
+
+def createHash():
+    secret_seed = str(random.randint(1, 1000000))
+    nonce = str(random.randint(1, 1000000))
+    hash_value = hashlib.sha256((secret_seed + nonce).encode()).hexdigest()
+    return secret_seed, nonce, hash_value
 
 @app.route('/')
 def home():
@@ -29,7 +47,6 @@ def price():
     cursor.execute('SELECT * FROM tbl_cryptos')
     results = cursor.fetchall()
     cursor.close()
-    print(results)
     return jsonify(results)
 
 @app.route('/balance', methods=['POST'])
@@ -37,28 +54,50 @@ def balance():
     data = request.get_json()
     # user_hash = data['hash']  #TODO : hash verify
     UserID = data['UserID']
-    print(UserID)
     cursor = mysql.connection.cursor()
     cursor.execute('SELECT * FROM tbl_users WHERE UserID=%s', (UserID,))
     results = cursor.fetchone()
     cursor.close()
-    print(results)
     ETH = results[12]
     BNB = results[13]
     return jsonify({"ETH":ETH, "BNB":BNB})
 
-@app.route('/get_hash', methods=['POST'])
-def verify_player():
+@app.route('/bet_coinflip', methods=['POST'])
+def bet_coinflip():
     data = request.get_json()
     # user_hash = data['hash']  #TODO : hash verify
     UserID = data['UserID']
+    coin_type = data['coin_type']
     bet_amount = data['bet_amount']
-
+    print("Bet Amount =================={}".format(bet_amount))
+    
     cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * FROM players WHERE UserID=%s", (user_Id))
-    player = cursor.fetchone()
-    print(player)
-    return jsonify({"server_hash" : "55"})
+    cursor.execute("SELECT * FROM tbl_users WHERE UserID=%s", (UserID,))
+    user = cursor.fetchone()
+
+    balance = 0
+    query = ""
+    if coin_type == 0 :
+        balance = user[12]
+        query = "UPDATE tbl_users SET ETH_Amount="
+    elif coin_type == 1 :
+        balance = user[13]
+        query = "UPDATE tbl_users SET BNB_Amount="
+    if balance >= bet_amount :
+        balance = balance - bet_amount
+    cursor.execute((query + "%s WHERE UserID=%s"), (balance, UserID,))
+    mysql.connection.commit()
+
+    secret_seed, nonce, server_hash = createHash()
+    token = generateToken(UserID)
+    cursor.execute("INSERT INTO tbl_coinflip (user_id, server_hash, secret_seed, nonce, parent_id, straight, is_expire_token, flip_result, win, winning_rate, cashout) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (UserID, server_hash, secret_seed, nonce, -1, 1, token, "", False, 0, 0,))
+    mysql.connection.commit()
+
+    cursor.execute("SELECT * FROM tbl_users WHERE UserID=%s", (UserID,))
+    user = cursor.fetchone()
+    cursor.close()
+        
+    return jsonify({"hash":server_hash, "ETH":user[12], "BNB":user[13]})
 
 
 if __name__ == '__main__':
